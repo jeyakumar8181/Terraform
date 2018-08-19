3fd0b8af163e5a0d64f04a7e256df0f00eb54073
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-08-19 21:22:18 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo yum -y install java-1.8.0-openjdk
+sudo yum -y install java-1.8.0-openjd
 sudo yum -y install wget
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
